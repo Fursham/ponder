7e0c0b7bcee486ceb97022d81b5367bed65bd355
“Fursham” <fursham.h@gmail.com>
2019-11-20 15:20:40 +0000

    Added warning suppression

--- R/matchSeqLevels.R
+++ R/matchSeqLevels.R
@@ -8,17 +8,19 @@
 #'
 matchSeqLevels <- function(inGRanges, refGRanges){
   
-  if (GenomeInfoDb::seqlevelsStyle(inGRanges) != GenomeInfoDb::seqlevelsStyle(refGRanges)) {
-    newStyle <- GenomeInfoDb::mapSeqlevels(GenomeInfoDb::seqlevels(inGRanges), GenomeInfoDb::seqlevelsStyle(refGRanges))
-    newStyle = newStyle[!is.na(newStyle)]
-    inGRanges <- GenomeInfoDb::renameSeqlevels(inGRanges, newStyle)
-    
-    if (any(!GenomeInfoDb::seqlevels(inGRanges)%in%GenomeInfoDb::seqlevels(refGRanges))) {
-      GenomeInfoDb::seqlevels(inGRanges, pruning.mode = 'tidy') <- as.vector(newStyle)
-      warnLog('Non-standard chromosome IDs in query were removed')
+  suppressWarnings(
+    if (GenomeInfoDb::seqlevelsStyle(inGRanges) != GenomeInfoDb::seqlevelsStyle(refGRanges)) {
+      newStyle <- GenomeInfoDb::mapSeqlevels(GenomeInfoDb::seqlevels(inGRanges), GenomeInfoDb::seqlevelsStyle(refGRanges))
+      newStyle = newStyle[!is.na(newStyle)]
+      inGRanges <- GenomeInfoDb::renameSeqlevels(inGRanges, newStyle)
+      
+      if (any(!GenomeInfoDb::seqlevels(inGRanges)%in%GenomeInfoDb::seqlevels(refGRanges))) {
+        GenomeInfoDb::seqlevels(inGRanges, pruning.mode = 'tidy') <- as.vector(newStyle)
+      }
+      
     }
-    
-  }
+  )
+  
   return(inGRanges)
 }
 
