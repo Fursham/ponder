a1dc74f3bfe22d656f5c9d1a47785e8964b3cfa7
“Fursham” <fursham.h@gmail.com>
2019-11-22 16:06:25 +0000

    Clean up test files

--- tests/testthat/test_exe.R
+++ tests/testthat/test_exe.R
@@ -2,12 +2,24 @@ context("Test prepNMDer workflow")
 
 # perform workflow
 testQuerynew = paste0('../../', testQuery)
-out = prepNMDer(testQuerynew, testRef, Mmusculus, match_chrom = T, match_geneIDs = T, primary_gene_id = 'gene_id', secondary_gene_id = 'ref_gene_id')
+out = suppressWarnings(prepNMDer(testQuerynew, testRef, Mmusculus, match_chrom = T, match_geneIDs = T, primary_gene_id = 'gene_id', secondary_gene_id = 'ref_gene_id'))
 test_that("Test the structure of output file", {
   
   expect_equal(isS4(out), TRUE) 
   expect_equal(length(slotNames(out)), 6)
-  expect_equal(names(out@df), c('NMDer_ID', 'Gene_ID', 'Original_Gene_ID', 
-                                'Match_level', 'Gene_name', 'Transcript_ID', 
-                                'Ref_transcript_ID', 'Transcript_coord', 'Strand'))
+  expect_equal(names(out@df), c('NMDer_ID', 'Gene_ID', 'Gene_name', 'Original_Gene_ID', 'Gene_match_level', 
+                                'Transcript_ID', 'Transcript_coord', 
+                                'Strand', 'Ref_transcript_ID'))
+})
+
+context("Test runNMDer workflow")
+
+# perform workflow
+out@df = out@df %>% dplyr::mutate(Coverage = 0,ORF_considered = as.character(NA), ORF_start = as.character('Not found'),ORF_found = FALSE)
+out.main = suppressWarnings(runMain(out@df, out@inputTranscripts, out@basicCDS, out@basicTranscripts, Mmusculus, testforNMD = T))
+test_that("Test the structure of output file", {
+  
+  expect_equal(names(out.main), names(runNMDer.out)) 
+  expect_equal(out.main$Coverage, runNMDer.out$Coverage)
+  expect_equal(out.main$is_NMD, runNMDer.out$is_NMD) 
 })
