35a508eeab4cd6ccf283aa0e6b196b0b944f1ed0
“Fursham” <fursham.h@gmail.com>
2019-11-22 11:55:47 +0000

    Removed unnecessaary function

--- R/getORF.R
+++ R/getORF.R
@@ -8,8 +8,8 @@
 #' @param knownCDS 
 #' @param queryTx 
 #' @param refsequence 
-#' @param PTC_dist 
-#' @param nonClassicalNMD 
+#' @param gene_id 
+#' @param transcript_id 
 #'
 #' @return
 #'
@@ -18,9 +18,8 @@ getORF <- function(knownCDS, queryTx, refsequence, gene_id, transcript_id) {
   
   # prep output list
   output = list(ORF_considered = as.character(NA),
-                Alt_tx  = as.logical(NA),
-                annotatedStart = as.logical(NA),
-                predictedStart = as.logical(NA))
+                ORF_start = as.character('Not found'),
+                ORF_found = FALSE)
   
   # precheck for annotated start codon on query transcript and update output
   pre_report = testTXforStart(queryTx, knownCDS, full.output=TRUE)
@@ -35,22 +34,10 @@ getORF <- function(knownCDS, queryTx, refsequence, gene_id, transcript_id) {
   if ((pre_report$annotatedStart == FALSE) |
       (pre_report$annotatedStart == TRUE & pre_report$firstexlen < 3)) {
     
-    diffSegments = pre_report$txrevise_out
-    queryStrand = as.character(strand(knownCDS))[1]
-    pre_report$ORF = sort(append(
-      diffSegments$shared_exons, c(
-        reduce(diffSegments[[1]][diffSegments[[1]]$upstream != TRUE]),
-        reduce(diffSegments[[2]][diffSegments[[2]]$upstream == TRUE]))),
-      decreasing = queryStrand == '-')
-    combinedList = list(refTx = pre_report$ORF, testTx = queryTx)
-    pre_report$txrevise_out =  indentifyAddedRemovedRegions("refTx", "testTx", combinedList[c("refTx", "testTx")])
-    
-    pre_report$predictedStart = 'reconstructed'
-    
-    #pre_report = reconstructCDSstart(queryTx, knownCDS,
-    #                                 refsequence,
-    #                                 pre_report$txrevise_out,
-    #                                 full.output = TRUE)
+    pre_report = reconstructCDSstart(queryTx, knownCDS,
+                                     refsequence,
+                                     pre_report$txrevise_out,
+                                     full.output = TRUE)
     output = utils::modifyList(output, list(predictedStart = pre_report$predictedStart))
     
     # return if CDS with new 5' do not contain a start codon
