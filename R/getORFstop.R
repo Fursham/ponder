d78d7128ce615a3fb3391b4a22c560e989c90947
Fursham <37694204+Fursham@users.noreply.github.com>
2019-11-24 22:36:06 +0000

    Made new script to scan for ORF stop and return 3UTRlength

---
+++ R/getORFstop.R
@@ -0,0 +1,30 @@
+getORFstop <- function(query, fasta, fiveUTRlength){
+  
+  output = list(ORF_found = FALSE,
+                threeUTRlength = 0)
+  
+  strand = as.character(strand(augmentedCDS))[1]
+  queryCDS = resizeTranscripts(query, start = fiveUTRlength)
+  queryseq = unlist(Biostrings::getSeq(fasta, queryCDS))
+  
+  # prepare a dict of stop codons for pattern matching
+  list_stopcodons = Biostrings::DNAStringSet(c("TAA", "TAG", "TGA"))
+  pdict_stopcodons = Biostrings::PDict(list_stopcodons)
+  
+  # search for in-frame stop codons
+  stopcodons = Biostrings::matchPDict(pdict_stopcodons, queryseq) %>% 
+    unlist()
+  stopcodons = sort(stopcodons[end(stopcodons) %% 3 == 0,])
+  
+  if(length(stopcodons) == 0){
+    return(output)
+  } else{
+
+    threeUTRlength = length(queryseq) - end(firststopcodon)
+    output$threeUTRlength = threeUTRlength
+    output$ORF_found = TRUE
+    
+    return(output)
+  }
+  
+}
