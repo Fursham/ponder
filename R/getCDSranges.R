03dd2308a52f3bab009cfacdfb96f9374bd1384a
“Fursham” <fursham.h@gmail.com>
2019-11-25 13:27:36 +0000

    Corrected variable name

--- R/getCDSranges.R
+++ R/getCDSranges.R
@@ -3,8 +3,8 @@ getCDSranges <- function(query, fiveUTRlength, threeUTRlength){
   CDSranges = resizeTranscripts(query, fiveUTRlength, threeUTRlength)
   CDSranges = CDSranges %>% as.data.frame() %>%
     dplyr::mutate(type = 'CDS', 
-                  gene_id = mcols(queryGRanges)$gene_id[1], 
-                  transcript_id = mcols(queryGRanges)$transcript_id[1]) %>%
+                  gene_id = mcols(query)$gene_id[1], 
+                  transcript_id = mcols(query)$transcript_id[1]) %>%
     dplyr::mutate(phase = cumsum(width%%3)%%3) %>%
     dplyr::select(seqnames:end, strand, type, phase, gene_id, transcript_id)
   CDSranges$phase = c(0, head(CDSranges$phase, - 1))
