1f27996a7dcd5478889244b97e0bd45d1de25b0d
“Fursham” <fursham.h@gmail.com>
2019-11-25 14:20:34 +0000

    Update comments

--- R/getORF.R
+++ R/getORF.R
@@ -1,32 +1,33 @@
 getORF <- function(knownCDS, queryTx, fasta) {
   
-  # prep output list
+  # prepare output list
   output = list(ORF_considered = as.character(NA),
                 ORF_start = as.character('Not found'),
                 fiveUTRlength = 0,
                 threeUTRlength = 0,
                 ORF_found = FALSE)
   
-  # attempt to get start codon
+  # attempt to find an aligned start codon
   report = getORFstart(queryTx, knownCDS, fasta)
-  output = utils::modifyList(output, report)
+  output = utils::modifyList(output, report) #update output
   
   # return if no start codon is found
   if(output$ORF_start == 'Not found'){
     return(output[c('ORF_considered', 'ORF_start', 'ORF_found')])
   }
   
-  # attempt to get stop codon
+  # attempt to search for an in-frame stop codon
   report = getORFstop(queryTx,  fasta, output$fiveUTRlength)
-  output = utils::modifyList(output, report)
+  output = utils::modifyList(output, report) #update output
   
   # return if no stop codon is found
   if(output$ORF_found == FALSE){
     return(output[c('ORF_considered', 'ORF_start', 'ORF_found')])
   }
   
+  # build new ORF Granges
   report = getCDSranges(queryTx, output$fiveUTRlength, output$threeUTRlength)
-  output = utils::modifyList(output, report)
+  output = utils::modifyList(output, report) #update output
   return(output[c('ORF_considered', 'ORF_start', 'ORF_found')])
   
 }
