eeb672feeaa18af1f4595321d251a213e589c058
“Fursham” <fursham.h@gmail.com>
2019-11-25 10:08:53 +0000

    Renamed files

--- R/getORF.R
+++ R/getORF.R
@@ -1,58 +1,33 @@
-#' _ workflow: Test transcript for NMD feature against a reference CDS
-#' 
-#' @description 
-#' THIS FUNCTION IS PART OF THE _ PROGRAM WORKFLOW.
-#' This function will compare the query transcript with a reference CDS and tests
-#' if insertion of alternative segments into the CDS generates an NMD substrate
-#'
-#' @param knownCDS 
-#' @param queryTx 
-#' @param refsequence 
-#' @param gene_id 
-#' @param transcript_id 
-#'
-#' @return
-#'
-#' @examples
-getORF <- function(knownCDS, queryTx, refsequence, gene_id, transcript_id) {
+getORF <- function(knownCDS, queryTx, fasta, gene_id, transcript_id) {
   
   # prep output list
   output = list(ORF_considered = as.character(NA),
                 ORF_start = as.character('Not found'),
+                fiveUTRlength = 0,
+                threeUTRlength = 0,
                 ORF_found = FALSE)
   
-  # precheck for annotated start codon on query transcript and update output
-  pre_report = testTXforStart(queryTx, knownCDS, full.output=TRUE)
-  output = utils::modifyList(output, pre_report["ORF_start"])
+  # attempt to get start codon
+  report = getORFstart(queryTx, knownCDS, fasta)
+  output = utils::modifyList(output, report)
   
-  # return if there is no shared exons between transcript and CDS
-  if (is.na(pre_report$txrevise_out[1])) {
-    return(output)
-  } 
+  # return if no start codon is found
+  if(output$ORF_start == 'Not_found'){
+    return(output[c('ORF_considered', 'ORF_start', 'ORF_found')])
+  }
   
-  # attempt to reconstruct CDS for transcripts with unannotated start
-  if ((pre_report$ORF_start == 'Not found') |
-      (pre_report$ORF_start == 'Annotated' & pre_report$firstexlen < 3)) {
-    
-    pre_report = reconstructCDSstart(queryTx, knownCDS,
-                                     refsequence,
-                                     pre_report$txrevise_out,
-                                     full.output = TRUE)
-    output = utils::modifyList(output, list(ORF_start = pre_report$ORF_start))
-    
-    # return if CDS with new 5' do not contain a start codon
-    if (pre_report$ORF_start == 'Not found'){
-      return(output)
-    }
-  } 
+  # attempt to get stop codon
+  report = getORFstop(queryTx,  fasta, output$fiveUTRlength)
+  output = utils::modifyList(output, report)
   
-  # reconstruct CDS with insertion of alternative segments
-  augmentedCDS = reconstructCDS(txrevise_out = pre_report$txrevise_out, 
-                                fasta = refsequence, 
-                                gene_id = gene_id, 
-                                transcript_id = transcript_id)
-  output = utils::modifyList(output, augmentedCDS)
+  # return if no stop codon is found
+  if(output$ORF_found == FALSE){
+    return(output[c('ORF_considered', 'ORF_start', 'ORF_found')])
+  }
   
-  return(output)
+  report = getCDSranges(queryTx, output$fiveUTRlength, output$threeUTRlength,
+                        gene_id, transcript_id)
+  output = utils::modifyList(output, report)
+  return(output[c('ORF_considered', 'ORF_start', 'ORF_found')])
+  
 }
-
