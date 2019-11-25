d41342e6bb09d8143acdf2fb99bf1f3d32da6e72
“Fursham” <fursham.h@gmail.com>
2019-11-25 14:39:10 +0000

    Updated comment

--- R/getORFranges.R
+++ R/getORFranges.R
@@ -3,6 +3,7 @@ getORFranges <- function(query, fiveUTRlength, threeUTRlength){
   # prepare output list
   output = list('ORF_considered' = NA)
   
+  # resize query GRanges to ORF and renew metadata info
   CDSranges = resizeTranscripts(query, fiveUTRlength, threeUTRlength)
   CDSranges = CDSranges %>% as.data.frame() %>%
     dplyr::mutate(type = 'CDS', 
@@ -12,7 +13,6 @@ getORFranges <- function(query, fiveUTRlength, threeUTRlength){
     dplyr::select(seqnames:end, strand, type, phase, gene_id, transcript_id)
   CDSranges$phase = c(0, head(CDSranges$phase, - 1))
   output$ORF_considered  = GenomicRanges::makeGRangesFromDataFrame(CDSranges, keep.extra.columns = TRUE)
-  
-  
+
   return(output)
 }
