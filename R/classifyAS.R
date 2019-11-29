c94490010242363b3897fd41ae4c2f61a0cb1e69
“Fursham” <fursham.h@gmail.com>
2019-11-29 10:43:59 +0000

    Completed new code for classifying alternative splicing

--- R/classifyAS.R
+++ R/classifyAS.R
@@ -1,57 +1,71 @@
-testAS <- function(tx1, tx2){
+#' Title
+#'
+#' @param tx1 GRanges object of exons by transcript 1
+#' @param tx2 GRanges object of exons by transcript 1
+#'
+#' @return GRanges object of alternative segments
+#' @export
+#'
+#' @examples
+classifyAS <- function(tx1, tx2){
   
-  #prepare output
-  output = as.character(NA)
-  
-  if(!any(tx1 %in% tx2)){
-    return()
+  # input checks
+  if (missing(tx1) | missing(tx2)) {
+    stop('Please provide input GRanges objects')
+  } else if(!any(tx1 %over% tx2)){
+    stop('Transcripts have no overlap')
   }
   
   # get information on transcripts
   tx1index = c(1:length(tx1))
   tx2index = c(length(tx1)+length(tx1)+length(tx2))
-  tx1ends = list('start' = 1, 
-                 'end' = length(tx1))
-  tx2ends = list('start' = length(tx1)+1, 
-                 'end' = length(tx1)+length(tx2))
-  txcomb = c(tx1ends, tx2ends)
   strand = strand(tx1)[1] %>% as.character()
 
-  # combine transcripts and disjoin
+  # combine transcripts and disjoin, annotate position of alt segments
   disjoint = BiocGenerics::append(tx1,tx2) %>%
     GenomicRanges::disjoin(with.revmap = T) %>%
     as.data.frame() %>%
     dplyr::mutate(type = ifelse(lengths(revmap)==2, 'cons','alt')) 
-  disjoint[1:min(which(lengths(disjoint$revmap)==2))-1,]$type = 'up'
-  disjoint[(max(which(lengths(disjoint$revmap)==2))+1):nrow(disjoint),]$type = 'down'
+  disjoint[1:min(which(disjoint$type == 'cons'))-1,]$type = 'up'
+  disjoint[(max(which(disjoint$type == 'cons'))+1):nrow(disjoint),]$type = 'down'
   
+  # prepare dataframe for classifcation
   disjoint = disjoint %>%
     dplyr::mutate(source = ifelse(type != 'cons' & revmap%in%tx1index, 1, 0)) %>%
     dplyr::mutate(source = ifelse(type != 'cons' & revmap%in%tx2index, 2, source)) %>%
-    dplyr::mutate(typesource = paste0(type,'_',source)) %>%
-    dplyr::mutate(typelag = dplyr::lag(type, default = 'start'), typelead = dplyr::lead(type, default = 'end')) %>%
-    dplyr::mutate(upcoord = dplyr::lag(end, default = .$start[1]), downcoord = dplyr::lead(start, default = .$end[dplyr::n()])) %>%
-    dplyr::mutate(updiff = start - upcoord, downdiff = downcoord-end)
-    
-    
-    dplyr::mutate(type = ifelse(type == 'alt' & revmap%in%txcomb, 'ends', type)) %>%
-    dplyr::mutate(type = ifelse(type == 'alt', 'internal', type)) %>%
-    dplyr::mutate(typesource = paste0(type,'_',source)) %>%
-    dplyr::mutate(sourcelag = dplyr::lag(typesource, default = 'start'), sourcelead = dplyr::lead(typesource, default = 'end')) %>%
+    dplyr::mutate(countersource = chartr("12", "21", source)) %>%
+    dplyr::mutate(sourcedown = dplyr::lag(source, default = 0), sourceup = dplyr::lead(source, default = 0)) %>%
     dplyr::mutate(upcoord = dplyr::lag(end, default = .$start[1]), downcoord = dplyr::lead(start, default = .$end[dplyr::n()])) %>%
     dplyr::mutate(updiff = start - upcoord, downdiff = downcoord-end)
   
+  # classify AS based on features
   disjoint = disjoint %>%
     dplyr::filter(type != 'cons') %>%
     dplyr::mutate(AS = NA) %>%
-    dplyr::mutate(AS = ifelse(sourcelag == 'start' & sourcelead == 'cons_0', 'ATS',AS)) %>%
-    dplyr::mutate(AS = ifelse(sourcelag == 'start' & sourcelead == paste0('alt_', ifelse(source == 1,2,1)), 'AF', AS)) %>%
-    dplyr::mutate(AS = ifelse(sourcelag != 'cons_0' & sourcelead == 'cons_0', 'AF',AS)) %>%
-    
-    dplyr::mutate(AS = ifelse(type == 'ends' & sourcelag == 'cons_0' & sourcelead == 'cons_0' & downdiff == 0, 'APA',AS)) %>%
-    dplyr::mutate(AS = ifelse(type == 'ends' & (sourcelag != 'cons_0' | sourcelead != 'cons_0') & updiff == 0, 'ATS',AS)) %>%
-    dplyr::mutate(AS = ifelse(type == 'ends' & (sourcelag != 'cons_0' | sourcelead != 'cons_0') & downdiff == 0, 'APA',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'up' & downdiff == 1, 'ts',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'up' & downdiff > 1, 'FE',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'down' &updiff == 1, 'pa',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'down' & updiff > 1, 'LE',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'alt' & updiff == 1 & downdiff == 1, 'RI',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'alt' & updiff == 1 & downdiff > 1, 'SD',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'alt' & updiff > 1 & downdiff == 1, 'SA',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'alt' & updiff > 1 & downdiff > 1 & (countersource %in% c(sourcedown, sourceup)), 'ME',AS)) %>%
+    dplyr::mutate(AS = ifelse(type == 'alt' & updiff > 1 & downdiff > 1 & (!countersource %in% c(sourcedown, sourceup)), 'CE',AS))
+  
+  # convert from alternative first exon to alternative (e.g.) if Ranges is on the neg strand
+  if(strand == '-'){
+    disjoint = disjoint %>%
+      dplyr::mutate(AS = chartr('DAFLtspa', 'ADLFpats', AS)) %>%
+      dplyr::arrange(dplyr::desc(start))
+  }
+  
+  # convert cases depending on whether segment is found in tx1 or tx2
+  #return as GRanges
+  disjoint = disjoint %>% 
+    dplyr::mutate(AS = ifelse(source == 2, toupper(AS), tolower(AS))) %>%
+    dplyr::select(seqnames:strand, AS) %>%
+    GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = T)
+  return(disjoint)
 
-    
 }
 
