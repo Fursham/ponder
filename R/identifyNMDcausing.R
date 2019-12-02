6f00aa70a240c31c13090b2f455272d1fabc9e27
“Fursham” <fursham.h@gmail.com>
2019-12-02 16:53:42 +0000

    Optimized code to identify NMDcausing exon

--- R/identifyNMDcausing.R
+++ R/identifyNMDcausing.R
@@ -1,7 +1,11 @@
 identifyNMDcausing <- function(ASgranges, cds){
   
+  output = list(NMDexon = NA, 
+                NMDexon.coord = NA)
+  
   tx1index = c(1:length(ASgranges))
   tx2index = c((length(ASgranges)+1):(length(ASgranges)+length(cds)))
+  lastcodingexonstart = start(cds[length(cds)])
   strand = strand(ASgranges)[1] %>% as.character()
   
 
@@ -12,30 +16,24 @@ identifyNMDcausing <- function(ASgranges, cds){
     dplyr::rowwise() %>% 
     dplyr::mutate(as = ifelse(any(revmap%in%tx1index), T,F)) %>%
     dplyr::mutate(cds = ifelse(any(revmap%in%tx2index), T,F)) %>%
-    dplyr::mutate(stop = ifelse(any(revmap%in%tail(tx2index,1)), T,F)) %>%
-    dplyr::mutate(olap = ifelse(lengths(revmap)==2, T,F)) %>%
+    dplyr::mutate(stop = ifelse(any(revmap%in%tail(tx2index,1)), 1,0)) %>%
+    dplyr::mutate(outframe = ifelse(width%%3 > 0, 1,0)) %>%
+    dplyr::mutate(dist = 0) %>%
+    dplyr::mutate(append = '') %>%
     as.data.frame()
   disjoint = disjoint[(min(which(disjoint$cds == T))):nrow(disjoint),]
-  
-  disjoint = disjoint %>%
-    dplyr::mutate(cdslead = dplyr::lead(cds, default = F)) %>%
-    
-  
-  disjoint[(max(which(disjoint$type == 'cds'))+1):nrow(disjoint),]$type = 'down'
-  
-    
-    dplyr::mutate(source = ifelse(revmap%in%tx1index, 1, 0)) %>%
-    dplyr::mutate(type = ifelse(revmap%in%tx2index, 'olap','nolap')) %>%
-    dplyr::mutate(source = ifelse(revmap%in%tx2index, 2, source))
+  disjoint[((max(which(disjoint$cds == T)))+1):nrow(disjoint),]$append = '3UTR-'
+  disjoint$dist = rank(replace(disjoint$start - lastcodingexonstart, which(disjoint$start - lastcodingexonstart < 0), NA), na.last = T)
   
   
-    dplyr::mutate(type = ifelse(lengths(revmap)==2, 'olap','nolap')) 
+  disjoint = disjoint %>% dplyr::filter(as == T) %>%
+    dplyr::arrange(dplyr::desc(stop), dplyr::desc(outframe), dist) %>%
+    dplyr::left_join(ASgranges %>% as.data.frame(), by = c("seqnames", "start", "end", "width", "strand")) %>%
+    dplyr::mutate(AS = paste0(append,AS)) %>%
+    dplyr::select(seqnames:strand,AS)
   
-  disjoint[1:min(which(disjoint$revmap %in% tx2index[1]))-1,]$type = 'up'
-  disjoint[(max(which(disjoint$revmap %in% tail(tx2index,1)))+1):nrow(disjoint),]$type = 'down'
-  
-  %>%
-    dplyr::mutate(source = ifelse(type != 'cons' & revmap%in%tx1index, 1, 0)) %>%
-    dplyr::mutate(source = ifelse(type != 'cons' & revmap%in%tx2index, 2, source))
-  
+  output$NMDexon = disjoint[1,]$AS
+  output$NMDexon.coord = paste0(disjoint[1,2:3], collapse = '-')
+
+  return(output)
 }
