d9571fcef0c4e14423e61bad6e98e793f2a4a64c
Fursham <37694204+Fursham@users.noreply.github.com>
2019-12-14 19:26:01 +0000

    Remove unused files

--- R/getCoverage.R
+++
@@ -1,9 +0,0 @@
-getCoverage <- function(tx1, tx2){
-  chrom = as.character(S4Vectors::runValue(GenomeInfoDb::seqnames(tx1)))
-  cov = GenomicRanges::coverage(c(tx1,tx2))
-  index = which(names(cov) == chrom)
-  cov = cov[[index]]
-  cov_val = S4Vectors::runValue(cov)
-  cov_len = S4Vectors::runLength(cov)
-  return(sum(cov_len[cov_val==2]) / ((sum(cov_len[cov_val==1]) + (sum(cov_len[cov_val==2])*2))/2))
-}
