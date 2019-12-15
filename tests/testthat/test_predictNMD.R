21bb837a79a540a1b5e7d51f7dd0ff2db8ed527d
Fursham <37694204+Fursham@users.noreply.github.com>
2019-12-15 13:50:47 +0000

    Modified tests

--- tests/testthat/test_predictNMD.R
+++ tests/testthat/test_predictNMD.R
@@ -1,7 +1,7 @@
 context("Test predictNMD functions")
 
-out1 = predictNMD(txl$ENSMUST00000029780,cdsl$ENSMUST00000029780)
-out2 = predictNMD(txl,cdsl, return = 'all')
+out1 = predictNMD(query_exons[[1]],query_cds[[1]])
+out2 = predictNMD(query_exons, query_cds, return = 'all')
 
 test_that("Test the structure of outputs", {
   
@@ -11,16 +11,15 @@ test_that("Test the structure of outputs", {
   expect_equal(names(out2), c('tx', 'is_NMD', 'dist_to_lastEJ',
                               'num_of_down_EJs', 'dist_to_downEJs',
                               'threeUTRlength')) 
-  expect_equal(nrow(out2), 6) 
+  expect_equal(nrow(out2), 4) 
 
 })
 
 test_that("Test the value of outputs", {
   
-  expect_equal(out2$dist_to_lastEJ, c(-133,361,-12,498,62,280)) 
-  expect_equal(out2$num_of_down_EJs, c(0,3,0,1,1,3)) 
-  expect_equal(out2$dist_to_downEJs, c("","66,283,361","",
-                                       "498","62","56,232,280")) 
-  expect_equal(out2$threeUTRlength, c(1586,502,343,736,788,381)) 
+  expect_equal(out2$dist_to_lastEJ, c(-133,-130,361,-129)) 
+  expect_equal(out2$num_of_down_EJs, c(0,0,3,0)) 
+  expect_equal(out2$dist_to_downEJs, c("","","66,283,361","")) 
+  expect_equal(out2$threeUTRlength, c(1586,1586,502,939)) 
 
 })
